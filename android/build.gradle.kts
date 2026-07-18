f4cce4bddb07e4c2907b0691f57e2a8f5a6e4005
KingAlexGilbert <Gandriel@proton.me>
2026-07-18 00:19:26 -0400

    Update Gradle Versions

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     // Match the Android Gradle Plugin used by the inspected APK metadata.
     // If your installed Android Studio uses a different AGP, keep your existing top-level version.
-    id("com.android.application") version "9.2.1" apply false
+    id("com.android.application") version "9.3.0" apply false
 }
